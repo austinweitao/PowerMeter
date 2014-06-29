bf9ab38d93341d78262eb29804c3ebb79c6edcb6
austinchen <austinchencareer@gmail.com>
2014-06-29 18:18:51 +0800

    recent commit

---
+++ curl-7.37.0/tests/configurehelp.pm
@@ -0,0 +1,23 @@
+# This is a generated file.  Do not edit.
+
+package configurehelp;
+
+use strict;
+use warnings;
+use Exporter;
+
+use vars qw(
+    @ISA
+    @EXPORT_OK
+    $Cpreprocessor
+    );
+
+@ISA = qw(Exporter);
+
+@EXPORT_OK = qw(
+    $Cpreprocessor
+    );
+
+$Cpreprocessor = 'gcc -E -I/lib/x86_64-linux-gnu//include -I/lib/x86_64-linux-gnu//include/openssl';
+
+1;
